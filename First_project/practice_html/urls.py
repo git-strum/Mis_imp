173f301a272ea21f86c0fb4146f3b6dba431d584
Manish Tripathi <ermani3007@gmail.com>
2025-07-19 19:00:56 +0530

    branch test successful

--- First_project/practice_html/urls.py
+++ First_project/practice_html/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns =[   
     path("welcome/", views.welcome, name = "welcome"),
     path("", views.welcome, name = "welcome"),
+    path("webp", views.webpage, name = "webp"),
 ]
--- First_project/practice_html/views.py
+++ First_project/practice_html/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def welcome(request):
     return HttpResponse("HI Dear")
+
+def webpage(request):
+    return render(request, "basic_web_page.html")
